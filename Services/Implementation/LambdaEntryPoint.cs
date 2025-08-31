aeeae35afd182d6f1c69166523b5ff5ad4df8c3a
Pedro Reisdorfer <pedroreisdorfer.dev@gmail.com>
2025-08-31 20:53:53 -0300

    - add  Amazon.Lambda.AspNetCoreServer

---
+++ Services/Implementation/LambdaEntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+
+namespace projetoGloboClima.Services.Implementation
+{
+    public class LambdaEntryPoint : APIGatewayProxyFunction
+    {
+
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseLambdaServer(); // Inicializa o WebApplication do Program.cs
+        }
+    }
+}
